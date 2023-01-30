315f28e716eb2d056750d549f42298ef04807dca
DarthChungo <antonio@adaro.eu>
2023-01-30 22:34:10 +0100

    Adde server type and version to server file

--- launch.sh
+++ launch.sh
@@ -6,7 +6,7 @@ mv config.cfg server/config.cfg
 cd /mc/server/
 . config.cfg
 
-[ ! -f "server.jar" ] && wget $SRV_URL -O server.jar
+[ ! -f "server-$MC_NAME-$MC_VER.jar" ] && wget $SRV_URL -O "server-$MC_NAME-$MC_VER.jar"
 echo "eula=true" > eula.txt
 
 cd /mc/
