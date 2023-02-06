c18d92a8d9a8def2b86cefe1b3fa157340fff04a
DarthChungo <antonio@adaro.eu>
2023-02-06 20:10:17 +0100

    Added new version options

--- entry.sh
+++ entry.sh
@@ -5,17 +5,57 @@
 get_download_url() {
   case $1 in
     "vanilla")
-      version="\"$2\""
+      if [ "$2" = "latest" ]; then
+        version_=$(wget "https://launchermeta.mojang.com/mc/game/version_manifest.json" -O- | jq -r ".latest.release")
+        version="\"$version_\""
+
+      elif [ "$2" = "latest-snapshot" ]; then
+        version_=$(wget "https://launchermeta.mojang.com/mc/game/version_manifest.json" -O- | jq -r ".latest.snapshot")
+        version="\"$version_\""
+
+      elif $(echo $2 | grep -Eq "[0-9]+\.[0-9]+\.[0-9]+"); then
+        version="\"$2\""
+      fi
+
       version_meta_url=$(wget -q "https://launchermeta.mojang.com/mc/game/version_manifest.json" -O- | jq -r ".versions | map({id , url}) | .[] | select(.id == $version).url")
       wget -q $version_meta_url -O- | jq -r ".downloads.server.url";;
 
     "fabric")
-      echo "https://meta.fabricmc.net/v2/versions/loader/$2/0.14.14/0.11.1/server/jar";;
+      if [ "$2" = "latest" ]; then
+        version=$(wget "https://meta.fabricmc.net/v2/versions" -O- | jq -r "[.game | .[] | select(.stable)][0].version")
+
+      elif [ "$2" = "latest-snapshot" ]; then
+        version=$(wget "https://meta.fabricmc.net/v2/versions" -O- | jq -r ".game[0].version")
+
+      elif $(echo $2 | grep -Eq "[0-9]+\.[0-9]+\.[0-9]+"); then
+        version=$2
+      fi
+
+      echo "https://meta.fabricmc.net/v2/versions/loader/$version/0.14.14/0.11.1/server/jar";;
 
     "forge")
-      version="\"$2-recommended\""
-      forge_version=$(wget -q "https://files.minecraftforge.net/net/minecraftforge/forge/promotions_slim.json" -O- | jq -r ".promos.$version")
-      echo "https://maven.minecraftforge.net/net/minecraftforge/forge/$2-$forge_version/forge-$2-$forge_version-installer.jar";;
+      if [ "$2" = "latest-recommended" ]; then
+        full_version=$(wget -q "https://files.minecraftforge.net/net/minecraftforge/forge/promotions_slim.json" -O- | jq -r '.promos | to_entries | map(select(.key | match(".{6}-recommended"))) | .[-1]')
+        version=$(echo $full_version | jq -r ".key" | cut -d- -f1)
+        forge_version=$(echo $full_version | jq -r ".value")
+
+      elif [ "$2" = "latest" ]; then
+        full_version=$(wget -q "https://files.minecraftforge.net/net/minecraftforge/forge/promotions_slim.json" -O- | jq -r '.promos | to_entries | map(select(.key | match(".{6}-latest"))) | .[-1]')
+        version=$(echo $full_version | jq -r ".key" | cut -d- -f1)
+        forge_version=$(echo $full_version | jq -r ".value")
+
+      elif $(echo $2 | grep -Eq "[0-9]+\.[0-9]+\.[0-9]+-latest"); then
+        version=$(echo $2 | cut -d- -f1)
+        version_name="\"$version-latest\""
+        forge_version=$(wget -q "https://files.minecraftforge.net/net/minecraftforge/forge/promotions_slim.json" -O- | jq -r ".promos.$version_name")
+
+      elif $(echo $2 | grep -Eq "[0-9]+\.[0-9]+\.[0-9]+") || $(echo $2 | grep -Eq "[0-9]+\.[0-9]+\.[0-9]+-recommended"); then
+        version=$(echo $2 | cut -d- -f1)
+        version_name="\"$version-recommended\""
+        forge_version=$(wget -q "https://files.minecraftforge.net/net/minecraftforge/forge/promotions_slim.json" -O- | jq -r ".promos.$version_name")
+      fi
+
+      echo "https://maven.minecraftforge.net/net/minecraftforge/forge/$version-$forge_version/forge-$version-$forge_version-installer.jar";;
   esac
 }
 
--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 cd /mc/server/
 
 if [ "$SERVER_TYPE" = "forge" ]; then
-  java $JVM_FLAGS $(cat libraries/net/minecraftforge/forge/$SERVER_VERSION-*/unix_args.txt)
+  java $JVM_FLAGS $(cat libraries/net/minecraftforge/forge/*/unix_args.txt)
 else
   java $JVM_FLAGS -jar server.jar nogui
 fi
